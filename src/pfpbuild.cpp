fe6f3fb7a6a4e38470c21fae7add2a303196734f
U-Ar <yuma.arakawa82128awakara.amuy@gmail.com>
2022-11-08 16:33:20 +0900

    reading .ssa & .esa

--- src/pfpbuild.cpp
+++ src/pfpbuild.cpp
@@ -43,15 +43,17 @@ public:
         // construct alphabet remapper (null character \0 is mapped to \1)
         idx.remap = std::vector<uchar>(256,0); idx.remap[0] = 1;
         idx.remap_inv = std::vector<uchar>(256,0); idx.remap_inv[1] = 0;
-        idx.sigma = 1;
+        idx.sigma = 0;
+        idx.F = std::vector<ulint>(256,0);
         {
-            std::vector<ulint> freqs(256,0); freqs[0] = 1;
+            std::vector<ulint> freqs(256,0);
             for (size_t i = 0; i < size; ++i) {
                 if (freqs[(uchar)fbwt.get()]++ == 0) idx.sigma++;
                 if (idx.sigma >= 255) {
                     die("Alphabet overflow");
                 }
             }
+            assert(freqs[0]==1);
             uchar new_c = 2;
             for (ulint c = 1; c < 256; ++c) {
                 if (freqs[(uchar)c] != 0) {
@@ -59,16 +61,72 @@ public:
                     idx.remap_inv[new_c++] = (uchar)c;
                 }
             }
+            // build F column
+            for (ulint i = 0; i < 256; ++i) idx.F[i] = freqs[remap_inv[i]];
+            for (ulint i = 255; i > 0; --i) idx.F[i] = idx.F[i-1];
+            idx.F[0] = 0;
+            for (ulint i = 1; i < 256; ++i) idx.F[i] += idx.F[i-1];
         }
 
         // build RLBWT with remapper
         fbwt.seekg(0);
         idx.bwt.load_from_plain(fbwt,size,idx.remap);
         fbwt.close();
+        assert(size==idx.bwt.size());
 
-        // read .ssa & .esa
-        FILE* file_ssa = open_aux_file(arg.input_file,EXTSSA,"rb");
-        FILE* file_esa = open_aux_file(arg.input_file,EXTESA,"rb");
+        r = idx.bwt.number_of_runs();
+        int log_r = bitsize(r);
+        int log_n = bitsize(size);
+
+        // read .ssa
+        FILE* file_ssa = open_aux_file(input,EXTSSA,"rb");
+
+        idx.samples_first = sdsl::int_vector<>(r,0,log_n);
+        idx.first_to_run = sdsl::int_vector<>(r,0,log_r);
+        {
+            auto pos_run_pairs = std::vector<std::pair<ulint,ulint>>(r);
+            ulint* sa_val;
+            for (ulint i = 0; i < r; ++i) {
+                size_t s = fread(sa_val,SABYTES,1,file_ssa);
+                if (s!=1) die(".ssa read failed");
+                samples_first[i] = *sa_val;
+                pos_run_pairs[i] = {*sa_val,i};
+            }
+            std::sort(pos_run_pairs.begin(),pos_run_pairs.end());
+
+            std::vector<ulint> first_pos;
+            for (ulint i = 0; i < r; ++i) {
+                first_pos.push_back(pos_run_pairs[i].first);
+                first_to_run[i] = pos_run_pairs[i].second;
+            }
+            idx.first = sparse_bitvector_t(first_pos.cbegin(),first_pos.cend());
+        }
+        fclose(file_ssa);
+
+        // read.esa
+        FILE* file_esa = open_aux_file(input,EXTESA,"rb");
+
+        idx.samples_last = sdsl::int_vector<>(r,0,log_n);
+        idx.last_to_run = sdsl::int_vector<>(r,0,log_r);
+        {
+            auto pos_run_pairs = std::vector<std::pair<ulint,ulint>>(r);
+            ulint* sa_val;
+            for (ulint i = 0; i < r; ++i) {
+                size_t s = fread(sa_val,SABYTES,1,file_esa);
+                if (s!=1) die(".esa read failed");
+                samples_last[i] = *sa_val;
+                pos_run_pairs[i] = {*sa_val,i};
+            }
+            std::sort(pos_run_pairs.begin(),pos_run_pairs.end());
+
+            std::vector<ulint> last_pos;
+            for (ulint i = 0; i < r; ++i) {
+                last_pos.push_back(pos_run_pairs[i].first);
+                last_to_run[i] = pos_run_pairs[i].second;
+            }
+            idx.last = sparse_bitvector_t(last_pos.cbegin(),last_pos.cend());
+        }
+        fclose(file_esa);
 
 
 
@@ -85,8 +143,23 @@ public:
         // build RLBWT^R with remapper
         idx.bwtR.load_from_plain(fbwt_rev,size,idx.remap);
         fbwt_rev.close();
+        assert(size==idx.bwtR.size());
 
+        rR = idx.bwtR.number_of_runs();
+        int log_rR = bitsize(rR);
 
+        // read .rev.ssa & .rev.esa
+        FILE* file_ssa_rev = open_aux_file(input_rev,EXTSSA,"rb");
+        FILE* file_esa_rev = open_aux_file(input_rev,EXTESA,"rb");
+
+        idx.samples_firstR = sdsl::int_vector<>(r,0,log_n);
+        idx.samples_lastR = sdsl::int_vector<>(r,0,log_n);
+        idx.first_to_runR = sdsl::int_vector<>(r,0,log_rR);
+        idx.last_to_runR = sdsl::int_vector<>(r,0,log_rR);
+        {
+            std::vector<ulint> first_posR;
+            std::vector<ulint> last_posR;
+        }
 
 
 
